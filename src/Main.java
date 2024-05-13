a88cb2a29e2ebb3addbf9aa171640ecdeb97394f
bikstok <jkjaerulff@gmail.com>
2024-05-13 12:40:35 +0200

    Added arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    ArrayList<Member> members = new ArrayList<>();
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
