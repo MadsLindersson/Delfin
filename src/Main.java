81c725185344437f15dd799e841227c19a588c22
MadsLindersson <Mali0002@stud.kea.dk>
2024-05-16 10:10:35 +0200

    Added loadDataFiles method in LoadData class.

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,6 @@ public class Main {
 
     public static ArrayList<Member> members = new ArrayList<>();
 
-    static {
-        CompetetiveMember member2 = new CompetetiveMember("Flemming", 22, 2, true, true, false, 0,
-                true, true, false, false, 0, 0, 0, 0, 0, 0, 0, 0);
-        members.add(member2);
-    }
     public static void main(String[] args) throws IOException {
         LoadData.loadDataFromFiles();
         MainMenu.menu();
