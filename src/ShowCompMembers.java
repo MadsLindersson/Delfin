c26f2eb734afcbdf1cc622c6b48e9ec499998906
Altagu <alexgunnersen25@gmail.com>
2024-05-21 11:18:38 +0200

    updated: ShowCompMembers, CompetetiveMembers.txt

--- src/ShowCompMembers.java
+++ src/ShowCompMembers.java
@@ -1,19 +1,18 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ShowCompMembers {
-    private static ArrayList<CompetetiveMember> juniorCrawlList = new ArrayList<>();
-    private static ArrayList<CompetetiveMember> juniorButterflyList = new ArrayList<>();
-    private static ArrayList<CompetetiveMember> juniorBackcrawlList = new ArrayList<>();
-    private static ArrayList<CompetetiveMember> juniorBreastList = new ArrayList<>();
+public final class ShowCompMembers {
+    public static ArrayList<CompetetiveMember> juniorCrawlList = new ArrayList<>();
+    public static ArrayList<CompetetiveMember> juniorButterflyList = new ArrayList<>();
+    public static ArrayList<CompetetiveMember> juniorBackcrawlList = new ArrayList<>();
+    public static ArrayList<CompetetiveMember> juniorBreastList = new ArrayList<>();
 
-    private static ArrayList<CompetetiveMember> seniorCrawlList = new ArrayList<>();
-    private static ArrayList<CompetetiveMember> seniorButterflyList = new ArrayList<>();
-    private static ArrayList<CompetetiveMember> seniorBackcrawlList = new ArrayList<>();
-    private static ArrayList<CompetetiveMember> seniorBreastList = new ArrayList<>();
+    public static ArrayList<CompetetiveMember> seniorCrawlList = new ArrayList<>();
+    public static ArrayList<CompetetiveMember> seniorButterflyList = new ArrayList<>();
+    public static ArrayList<CompetetiveMember> seniorBackcrawlList = new ArrayList<>();
+    public static ArrayList<CompetetiveMember> seniorBreastList = new ArrayList<>();
 
-    public ShowCompMembers() {
-    }
+    private ShowCompMembers() {}
 
     public static void sortCompMembers() {
         for (Member member : Main.members) {
@@ -81,6 +80,7 @@ public class ShowCompMembers {
                                         System.out.println("---------------------------------------");
                                     }
                                 }
+                                break;
                             case 2:
                                 for (CompetetiveMember member : juniorButterflyList) {
                                     if (member.isButterfly()) {
@@ -95,6 +95,7 @@ public class ShowCompMembers {
                                         System.out.println("---------------------------------------");
                                     }
                                 }
+                                break;
                             case 3:
                                 for (CompetetiveMember member : juniorBackcrawlList) {
                                     if (member.isCrawl()) {
@@ -109,6 +110,7 @@ public class ShowCompMembers {
                                         System.out.println("---------------------------------------");
                                     }
                                 }
+                                break;
                             case 4:
                                 for (CompetetiveMember member : juniorBreastList) {
                                     if (member.isCrawl()) {
@@ -123,6 +125,7 @@ public class ShowCompMembers {
                                         System.out.println("---------------------------------------");
                                     }
                                 }
+                                break;
                             case 5:
                                 listOfCompetetiveMembers();
                             default:
@@ -130,79 +133,85 @@ public class ShowCompMembers {
                         }
                     }
                 case 2:
-                    System.out.println("""
-                            **Senior teams**
-                            1. Crawl team
-                            2. Butterfly team
-                            3. Backcrawl team
-                            4. Breast team""");
-                    int choice = sc.nextInt();
-                    switch (choice) {
-                        case 1:
-                            for (CompetetiveMember member : seniorCrawlList) {
-                                if (member.isCrawl()) {
-                                    System.out.println("Sr. Crawl team member:");
-                                    System.out.println("Name: " + member.getName());
-                                    System.out.println("Age: " + member.getAge());
-                                    System.out.println("ID: " + member.getId());
+                    boolean c2 = true;
+                    while (c2) {
+                        System.out.println("""
+                                **Senior teams**
+                                1. Crawl team
+                                2. Butterfly team
+                                3. Backcrawl team
+                                4. Breast team
+                                5. Go back""");
+                        int choice = sc.nextInt();
+                        switch (choice) {
+                            case 1:
+                                for (CompetetiveMember member : seniorCrawlList) {
+                                    if (member.isCrawl()) {
+                                        System.out.println("Sr. Crawl team member:");
+                                        System.out.println("Name: " + member.getName());
+                                        System.out.println("Age: " + member.getAge());
+                                        System.out.println("ID: " + member.getId());
 
-                                    System.out.println("Best Training Crawl Time: " + member.getBestCrawlTime());
-                                    System.out.println("Competition Crawl Time: " + member.getCompetitionCrawlTime());
+                                        System.out.println("Best Training Crawl Time: " + member.getBestCrawlTime());
+                                        System.out.println("Competition Crawl Time: " + member.getCompetitionCrawlTime());
 
-                                    System.out.println("---------------------------------------");
+                                        System.out.println("---------------------------------------");
+                                    }
                                 }
-                            }
-                            break;
-                        case 2:
-                            for (CompetetiveMember member : seniorButterflyList) {
-                                if (member.isButterfly()) {
-                                    System.out.println("Sr. Butterfly team member:");
-                                    System.out.println("Name: " + member.getName());
-                                    System.out.println("Age: " + member.getAge());
-                                    System.out.println("ID: " + member.getId());
+                                break;
+                            case 2:
+                                for (CompetetiveMember member : seniorButterflyList) {
+                                    if (member.isButterfly()) {
+                                        System.out.println("Sr. Butterfly team member:");
+                                        System.out.println("Name: " + member.getName());
+                                        System.out.println("Age: " + member.getAge());
+                                        System.out.println("ID: " + member.getId());
 
-                                    System.out.println("Best Training Crawl Time: " + member.getBestButterflyTime());
-                                    System.out.println("Competition Crawl Time: " + member.getCompetitionButterflyTime());
+                                        System.out.println("Best Training Crawl Time: " + member.getBestButterflyTime());
+                                        System.out.println("Competition Crawl Time: " + member.getCompetitionButterflyTime());
 
-                                    System.out.println("---------------------------------------");
+                                        System.out.println("---------------------------------------");
+                                    }
                                 }
-                            }
-                            break;
-                        case 3:
-                            for (CompetetiveMember member : seniorBackcrawlList) {
-                                if (member.isCrawl()) {
-                                    System.out.println("Sr. Backcrawl team member:");
-                                    System.out.println("Name: " + member.getName());
-                                    System.out.println("Age: " + member.getAge());
-                                    System.out.println("ID: " + member.getId());
+                                break;
+                            case 3:
+                                for (CompetetiveMember member : seniorBackcrawlList) {
+                                    if (member.isCrawl()) {
+                                        System.out.println("Sr. Backcrawl team member:");
+                                        System.out.println("Name: " + member.getName());
+                                        System.out.println("Age: " + member.getAge());
+                                        System.out.println("ID: " + member.getId());
 
-                                    System.out.println("Best Training Crawl Time: " + member.getBestBackcrawlTime());
-                                    System.out.println("Competition Crawl Time: " + member.getCompetitionBackcrawlTime());
+                                        System.out.println("Best Training Crawl Time: " + member.getBestBackcrawlTime());
+                                        System.out.println("Competition Crawl Time: " + member.getCompetitionBackcrawlTime());
 
-                                    System.out.println("---------------------------------------");
+                                        System.out.println("---------------------------------------");
+                                    }
                                 }
-                            }
-                            break;
-                        case 4:
-                            for (CompetetiveMember member : seniorBreastList) {
-                                if (member.isCrawl()) {
-                                    System.out.println("Sr. Breast team member:");
-                                    System.out.println("Name: " + member.getName());
-                                    System.out.println("Age: " + member.getAge());
-                                    System.out.println("ID: " + member.getId());
+                                break;
+                            case 4:
+                                for (CompetetiveMember member : seniorBreastList) {
+                                    if (member.isCrawl()) {
+                                        System.out.println("Sr. Breast team member:");
+                                        System.out.println("Name: " + member.getName());
+                                        System.out.println("Age: " + member.getAge());
+                                        System.out.println("ID: " + member.getId());
 
-                                    System.out.println("Best Training Crawl Time: " + member.getBestBreastTime());
-                                    System.out.println("Competition Crawl Time: " + member.getCompetitionBreastTime());
+                                        System.out.println("Best Training Crawl Time: " + member.getBestBreastTime());
+                                        System.out.println("Competition Crawl Time: " + member.getCompetitionBreastTime());
 
-                                    System.out.println("---------------------------------------");
+                                        System.out.println("---------------------------------------");
+                                    }
                                 }
-                            }
-                            break;
-                        default:
-                            System.out.println("Invalid choice");
-                            break;
+                                break;
+                            case 5:
+                                c2 = false;
+                                break;
+                            default:
+                                System.out.println("Invalid choice");
+                        }
+                        break;
                     }
-                    break;
                 case 3:
                     running = false;
                     break;
